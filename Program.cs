ccba0fef751e63dd42d0f88d454e78e080019ea9
Lukas Selin <lukas.selin@viskan.se>
2019-09-23 17:34:38 +0200

    initial commit with some basic framework

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace battleship
+{
+    class Program
+    {   
+        static void startNewGame()
+        {
+            string menuHeader = "Start new game";
+            string[] menuContent = {"Launch game", "Settings", "Go back"};
+            
+            var menu = new Menu(menuContent);
+            menu = menu.GetMenu(menu, menuHeader);
+
+            switch(menu.SelectedIndex)
+            {
+                case 0:
+                // Launch game
+
+                break;
+
+                case 1:
+                // Settings
+                    throw new Exception("Not yet implemented");
+
+                case 2:
+                // Go back
+                    launchGameMenu();
+                break;
+
+                default:
+                // Wrong
+
+                break;
+
+            }
+        }
+        static void launchGameMenu()
+        {
+            string menuHeader = "Battleship";
+            string[] menuContent = {"New game", "Load game", "Leaderboard", "Exit"};
+
+            var menu = new Menu(menuContent);
+
+            menu = menu.GetMenu(menu, menuHeader);
+
+            switch(menu.SelectedIndex)
+            {
+                case 0:
+                    // Nytt spel
+
+                break;
+
+                case 1:
+                    // Ladda spel
+
+                break;
+
+                case 2:
+                    // Leaderboard
+
+                break;
+
+                case 3:
+                    // Avsluta
+                    Console.ResetColor();
+                    return;
+
+                default:
+                    // Fel hanterig
+
+                break;
+            }
+        }
+        static void Main(string[] args)
+        {            
+            launchGameMenu();
+        }
+    }
+}
